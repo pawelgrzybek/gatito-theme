7a1f1f631c4a966c5e80741f654017a81964df7c
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2019-07-31 15:15:13 +0100

    Input Control

--- src/colors/inputControl.js
+++ src/colors/inputControl.js
@@ -4,18 +4,18 @@ const theme = require("../theme");
 // https://code.visualstudio.com/docs/getstarted/theme-color-reference#_input-control
 
 module.exports = {
-  "input.background": theme.backgroundDark,
-  "input.border": "#30373A",
+  "input.background": theme.background,
+  "input.border": theme.backgroundLight,
   "input.foreground": theme.foreground,
   "input.placeholderForeground": theme.foregroundDark,
   "inputOption.activeBorder": theme.foregroundDark,
-  // "inputValidation.infoForeground": "#f00",
-  // "inputValidation.warningForeground": "#f00",
-  // "inputValidation.errorForeground": "#f00",
-  "inputValidation.errorBackground": "#A94348",
-  "inputValidation.errorBorder": "#30373A",
-  "inputValidation.infoBackground": "#A94348",
-  "inputValidation.infoBorder": "#30373A",
-  "inputValidation.warningBackground": "#A94348",
-  "inputValidation.warningBorder": "#30373A"
+  "inputValidation.errorBackground": theme.red,
+  "inputValidation.errorForeground": theme.foreground,
+  "inputValidation.errorBorder": theme.red,
+  "inputValidation.infoBackground": theme.blue,
+  "inputValidation.infoForeground": theme.foreground,
+  "inputValidation.infoBorder": theme.blue,
+  "inputValidation.warningBackground": theme.yellow,
+  "inputValidation.warningForeground": theme.background,
+  "inputValidation.warningBorder": theme.yellow
 };
