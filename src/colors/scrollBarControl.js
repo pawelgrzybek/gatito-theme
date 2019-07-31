4e276cb19344dbb5626037ab748c9f864165fc8a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2019-07-31 15:21:01 +0100

    Scroll Bar Control

--- src/colors/scrollBarControl.js
+++ src/colors/scrollBarControl.js
@@ -5,7 +5,7 @@ const theme = require("../theme");
 
 module.exports = {
   "scrollbar.shadow": theme.transparent,
-  "scrollbarSlider.activeBackground": "#FFFFFF2E",
+  "scrollbarSlider.activeBackground": theme.backgroundLight,
   "scrollbarSlider.background": "#FFFFFF0F",
   "scrollbarSlider.hoverBackground": "#FFFFFF1F"
 };
