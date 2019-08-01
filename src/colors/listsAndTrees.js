02c32ff834320c39cee5464103a27f70f136c685
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2019-08-01 10:47:05 +0200

    Lists and Trees

--- src/colors/listsAndTrees.js
+++ src/colors/listsAndTrees.js
@@ -4,19 +4,22 @@ const theme = require("../theme");
 // https://code.visualstudio.com/docs/getstarted/theme-color-reference#_lists-and-trees
 
 module.exports = {
-  "list.activeSelectionBackground": "#30373A",
+  "list.activeSelectionBackground": theme.backgroundLight,
   "list.activeSelectionForeground": theme.foreground,
-  "list.dropBackground": "#30373A",
-  "list.focusBackground": "#30373A",
-  "list.highlightForeground": theme.foreground,
-  "list.hoverBackground": "#30373A",
-  "list.inactiveSelectionBackground": "#30373A",
-  "list.inactiveSelectionForeground": theme.foreground,
-  "list.hoverForeground": theme.foreground,
+  "list.dropBackground": theme.backgroundLight,
+  "list.focusBackground": theme.backgroundLight,
   "list.focusForeground": theme.foreground,
+  "list.highlightForeground": theme.foreground,
+  "list.hoverBackground": theme.backgroundLight,
+  "list.hoverForeground": theme.foreground,
+  "list.inactiveSelectionBackground": theme.backgroundLight,
+  "list.inactiveSelectionForeground": theme.foreground,
+  "list.inactiveFocusBackground": theme.backgroundLight,
+  "list.invalidItemForeground": theme.red,
   "list.errorForeground": theme.red,
   "list.warningForeground": theme.yellow,
   "listFilterWidget.background": theme.background,
-  "listFilterWidget.outline": "#30373A",
-  "listFilterWidget.noMatchesOutline": theme.red
+  "listFilterWidget.outline": theme.backgroundLight,
+  "listFilterWidget.noMatchesOutline": theme.red,
+  "tree.indentGuidesStroke": theme.backgroundLight
 };
